e6e486f82a03ae1d1db4d22625aaa5a190d9e0f9
Yvonne <ywang726@uw.edu>
2024-11-21 09:00:55 -0800

    added messageFromYvonne method

--- Names.java
+++ Names.java
@@ -3,8 +3,15 @@ public class Names {
   public static void secretMessage() {
     System.out.println("Hello!");
   }
-  
+
+  public static void messageFromYvonne() {
+    System.out.println("tired of coding but this is fun :)");
+  }
+
   public static void main(String[] args) {
     secretMessage();
+    messageFromYvonne();
   }
+
+
 }
