3f1ae07dadb434bb621e725b883f2fc2f7b63ff1
paullyoon <pyoon@uw.edu>
2024-11-21 08:57:40 -0800

    added paulHello method

--- Names.java
+++ Names.java
@@ -3,8 +3,13 @@ public class Names {
   public static void secretMessage() {
     System.out.println("Hello!");
   }
+
+  public static void paulHello() {
+    System.out.println("Hiii!");
+  }
   
   public static void main(String[] args) {
     secretMessage();
+    paulHello();
   }
 }
