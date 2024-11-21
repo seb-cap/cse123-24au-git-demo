cb3ae41e36d758aacf99789367163d62b1f5ff76
Drivthrucover <priank@outlook.com>
2024-11-21 09:03:18 -0800

    added priank method

--- Names.java
+++ Names.java
@@ -3,8 +3,14 @@ public class Names {
   public static void secretMessage() {
     System.out.println("Hello!");
   }
+
+  public static void priankHello()
+  {
+    System.out.println("Happy Thursday!");
+  }
   
   public static void main(String[] args) {
     secretMessage();
+    priankHello();
   }
 }
