ce56f4d2ae8a34a3c85ce34a5bd59e63bd80abe2
droag <jewf.games@gmail.com>
2024-11-21 09:00:46 -0800

    Added a function to print a left-aligned triangle with asterisks.

--- Names.java
+++ Names.java
@@ -3,8 +3,18 @@ public class Names {
   public static void secretMessage() {
     System.out.println("Hello!");
   }
+
+  public static void makeTriangle(int rows){
+    for (int row = 0; row < rows; row++){
+      for (int col = 0; col <= row; col++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
   
   public static void main(String[] args) {
     secretMessage();
+    makeTriangle(4);
   }
 }
