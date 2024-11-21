667691e3b541d52585c56da164edf2554aedf3af
jennybuii <jenny12345648@gmail.com>
2024-11-21 09:01:14 -0800

    added Jenny message

--- Names.java
+++ Names.java
@@ -3,8 +3,14 @@ public class Names {
   public static void secretMessage() {
     System.out.println("Hello!");
   }
+
+  public static void sayHey(){
+    System.out.println("Hey");
+  }
   
   public static void main(String[] args) {
     secretMessage();
+    sayHey();
   }
+
 }
