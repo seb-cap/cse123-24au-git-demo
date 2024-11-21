6bd24e72494fae1607ec8777aa3f71040b8cb34f
Sebastian <sebcap@uw.edu>
2024-11-21 08:57:42 -0800

    added sebastian method

--- Names.java
+++ Names.java
@@ -3,8 +3,13 @@ public class Names {
   public static void secretMessage() {
     System.out.println("Hello!");
   }
+
+  public static void sebastianHello() {
+    System.out.println("Happy THursday!");
+  }
   
   public static void main(String[] args) {
     secretMessage();
+    sebastianHello();
   }
 }
